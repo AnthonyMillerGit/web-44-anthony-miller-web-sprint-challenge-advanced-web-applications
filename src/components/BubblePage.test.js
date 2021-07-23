6ef9aa5566c5c4412f4b1784ccb11cf5141a2b30
ferengitooth <ehitsamill@gmail.com>
2021-07-22 22:02:16 -0700

    bubblepage test

--- src/components/BubblePage.test.js
+++ src/components/BubblePage.test.js
@@ -1,13 +1,54 @@
-import React from 'react';
-import MutationObserver from 'mutationobserver-shim';
+import React from "react";
+import { render, screen, waitFor } from "@testing-library/react";
+import BubblePage from "./BubblePage";
 
-import { render, screen} from "@testing-library/react";
-import BubblePage from './BubblePage';
+import {fetchColors as mockFetchColors} from './fetchColors'
+jest.mock('./fetchColors')
 
-test("Renders without errors", ()=> {
-    
+const mockColors = [
+    {
+    color: "aliceblue",
+    code: {
+    hex: "#f0f8ff",
+    },
+    id: 1,
+},
+    {
+    color: "limegreen",
+    code: {
+    hex: "#99ddbc",
+    },
+    id: 2,
+    }
+]
+
+test("Renders BubblePage without errors", () => {
+    render(<BubblePage/>)
 });
 
-test("Renders appropriate number of colors passed in through mock", async ()=> {
-    //Keep in mind that our service is called on mount for this component.
+test("Fetches data and renders the bubbles on mounting", async () => {
+    render(<BubblePage/>)
+    mockFetchColors.mockResolvedValueOnce({
+    colors: [
+        {
+          color: "aliceblue",
+          code: {
+            hex: "#f0f8ff",
+          },
+          id: 1,
+        },
+        {
+          color: "limegreen",
+          code: {
+            hex: "#99ddbc",
+          },
+          id: 2,
+        }
+    ]
+  })
+
+  await waitFor(() => {
+    const color = screen.findAllByTestId('color-test')
+    expect(color).toBeInTheDocument()
+  })
 });
