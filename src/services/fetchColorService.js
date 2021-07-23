fee90baf90528875f928efbcff8b39911e7ea483
ferengitooth <ehitsamill@gmail.com>
2021-07-23 14:56:26 -0700

    passing 14 out of the 15 tests. fleshed out fetchcolorservice.

--- src/services/fetchColorService.js
+++ src/services/fetchColorService.js
@@ -1,7 +1,13 @@
-import axiosWithAuth from '../helpers/axiosWithAuth';
+import { axiosWithAuth } from '../helpers/axiosWithAuth';
 
 const fetchColorService = () => {
-    
+    return axiosWithAuth().get('http://localhost:5000/api/colors')
+            .then(res => {
+                return res
+            })
+            .catch(err => {
+                console.log('err: ', err)
+            })
 }
 
 export default fetchColorService;
