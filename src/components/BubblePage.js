4fc1e6fba3314380a475b895b2e63cc4efb082f4
ferengitooth <ehitsamill@gmail.com>
2021-07-23 13:56:46 -0700

    messing around with testing still.

--- src/components/BubblePage.js
+++ src/components/BubblePage.js
@@ -1,36 +1,63 @@
 import React, { useEffect, useState } from "react";
-import axiosWithAuth from "../helpers/axiosWithAuth";
-
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
+import fetchColorService from '../services/fetchColorService';
+import { axiosWithAuth } from "../helpers/axiosWithAuth";
 
 const BubblePage = () => {
-  const [colorList, setColorList] = useState([]);
+  const [colors, setColors] = useState([]);
+  const [editing, setEditing] = useState(false);
 
-  useEffect(() => {
+  useEffect( () =>{
+    fetchColorService()
+      .then(res => {
+        setColors(res.data)
+      })
+      .catch(err => {
+        console.log('err', err)
+      })
+  },[])
+  const toggleEdit = (value) => {
+    setEditing(value);
+  };
 
-    axiosWithAuth()
-    .get(`/colors`)
-    .then((res) => {
-      setColorList(res.data)
-    })
-    .catch((err) => {
-      console.log(err)
-    })
+  const saveEdit = (editColor) => {
+    axiosWithAuth().put(`http://localhost:5000/api/colors/${editColor.id}`, editColor)
+      .then(res => {
+        const newColors = colors.map(color => {
+          if(color.id === editColor.id) {
+            return editColor
+          } else {
+            return color
+          }
+        })
+        setColors(newColors)
+      })
+      .catch(err => {
+        console.log('err:', err)
+      })
+  };
 
-  }, [])
+  const deleteColor = (colorToDelete) => {
+    axiosWithAuth().delete(`http://localhost:5000/api/colors/${colorToDelete.id}`)
+      .then(res => {
+        const newColors = colors.filter(color => color.id !== JSON.parse(res.data))
+        setColors(newColors)
+      })
+      .catch(err => {
+        console.log('err: ', err)
+      })
+  };
 
   return (
     <div className="container">
-      <ColorList colors={colorList} updateColors={setColorList} />
-      <Bubbles colors={colorList} />
+      <ColorList colors={colors} editing={editing} toggleEdit={toggleEdit} saveEdit={saveEdit} deleteColor={deleteColor}/>
+      <Bubbles colors={colors}/>
     </div>
   );
 };
 
 export default BubblePage;
-
-
 //Task List:
 //1. When the component mounts, make an axios call to retrieve all color data and push to state.
 //2. Complete toggleEdit, saveEdit, deleteColor and functions
--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -1,62 +1,19 @@
 import React, { useState } from "react";
 import Color from './Color';
 import EditMenu from './EditMenu';
-import axiosWithAuth from '../helpers/axiosWithAuth'
 
-const initialColor = {
-  color: "",
-  code: { hex: "" }
-};
-
-const ColorList = ({ colors, updateColors }) => {
-  const [editing, setEditing] = useState(false);
-  const [colorToEdit, setColorToEdit] = useState(initialColor);
-
-  const editColor = color => {
-    setEditing(true);
-    setColorToEdit(color);
-  };
-
-  const saveEdit = e => {
-    e.preventDefault();
-    axiosWithAuth()
-    .put(`/colors/:id`, colorToEdit)
-    .then((res) => {
-      updateColors (
-        colors.map((color) => {
-          if(color.id === res.data.id) {
-            return res.data
-          } else {
-            return color
-          }
-        })
-      )
-    })
-    .catch((err) => {
-      console.log(err)
-    })
-  };
-
-  const deleteColor = color => {
-    axiosWithAuth()
-    .delete(`/colors/${color.id}`)
-    .then((res) => {
-      updateColors(colors.filter((col) => col.id !== Number(res.data)))
-    })
-    .catch((error) => {
-      console.log(error)
-    })
-  };
+const ColorList = (props) => {
+  const { colors, editing, toggleEdit, saveEdit, deleteColor } = props;
+  const [ editColor, setEditColor] = useState({ color: "", code: { hex: "" }});
 
   return (
     <div className="colors-wrap">
-      <p>colors</p>
+      <p id="color_title">colors</p>
       <ul>
-        {colors.map(color => <Color key={color.id} editing={editing} color={color} editColor={editColor} deleteColor={deleteColor}/>)}
+        {colors.map(color => <Color key={color.id} setEditColor={setEditColor} color={color} toggleEdit={toggleEdit} deleteColor={deleteColor}/>)}
       </ul>
       
-      { editing && <EditMenu colorToEdit={colorToEdit} saveEdit={saveEdit} setColorToEdit={setColorToEdit} setEditing={setEditing}/> }
-
+      {editing && <EditMenu editColor={editColor} setEditColor={setEditColor} toggleEdit={toggleEdit} saveEdit={saveEdit}/>}
     </div>
   );
 };
